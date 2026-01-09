d48a5de2cf661d4145514e4329b8dea88474c35a
David Santos <david.santos@finanblue.com.vc>
2026-01-09 17:28:30 -0300

    backup, continuar depois

---
+++ src/FCG.Core/Objects/RetrySettings.cs
@@ -0,0 +1,8 @@
+﻿namespace FCG.Core.Objects
+{
+    public static class RetrySettings
+    {
+        public static int MaxRetryAttempts { get; set; }
+        public static int DelayBetweenRetriesInSeconds { get; set; }
+    }
+}
---
+++ src/FCG.Payments/Configuration/RetrySettingsInit.cs
@@ -0,0 +1,12 @@
+﻿using FCG.Core.Objects;
+
+namespace FCG.Payments.Configuration;
+
+public static class RetrySettingsInit
+{
+    public static void InitilizeRetrySettings(this HostApplicationBuilder builder)
+    {
+        RetrySettings.MaxRetryAttempts = builder.Configuration.GetValue<int>("RetrySettings:MaxRetryAttempts");
+        RetrySettings.DelayBetweenRetriesInSeconds = builder.Configuration.GetValue<int>("RetrySettings:DelayBetweenRetriesInSeconds");
+    }
+}
